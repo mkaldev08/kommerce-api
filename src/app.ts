73d89759c7ba654005faa8068614e2fe35adff70
Manuel Kalueka <pedrokalueca@gmail.com>
2026-05-26 16:16:42 +0100

    fix: conditionally set logger level based on NODE_ENV

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import { initializeFinancialPlansCronJob } from './lib/cron-jobs'
 
 export const app = fastify({
   logger: {
-    level: env.LOG_LEVEL,
+    level: env.NODE_ENV === 'dev' ? env.LOG_LEVEL : '',
   },
 })
 const API_VERSION = '/api/v1'
